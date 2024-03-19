c6947ed91b3126cbda6f9e877a877bb09a9673d1
Christopher Hiller <boneskull@boneskull.com>
2024-03-19 13:27:20 -0700

    chore: update eslint config for js files

--- .config/eslint-js.config.js
+++ .config/eslint-js.config.js
@@ -1,13 +1,15 @@
 // @ts-check
 
 import stylisticJs from '@stylistic/eslint-plugin-js';
+import tseslint from 'typescript-eslint';
 
 // TODO: figure out what type to use here
 export default {
   plugins: {
     '@stylistic/js': stylisticJs,
   },
-  files: ['**/*.js', '**/*.cjs'],
+  files: ['**/*.js', '**/*.cjs', '*.cjs', '*.js'],
+  extends: [tseslint.configs.disableTypeChecked],
   rules: {
     '@stylistic/js/semi': ['error', 'always'],
     '@stylistic/js/lines-around-comment': [
