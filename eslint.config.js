3a3e0fe5568d80f9cb7e10a7f78f7256fab98558
Christopher Hiller <boneskull@boneskull.com>
2024-04-02 15:18:21 -0700

    chore(eslint): ditch separate config files for strict typing problems

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 // @ts-check
 
+import stylisticJs from '@stylistic/eslint-plugin-js';
+import stylisticTs from '@stylistic/eslint-plugin-ts';
 import eslintPluginJsonc from 'eslint-plugin-jsonc';
 import tseslint from 'typescript-eslint';
-import jsConfig from './.config/eslint-js.config.js';
-import tsConfig from './.config/eslint-ts.config.js';
 
 // TODO: setup eslint-plugin-jsonc & eslint-plugin-n
 export default tseslint.config(
@@ -17,9 +17,92 @@ export default tseslint.config(
       },
     },
   },
-  // @ts-expect-error - FIXME
-  tsConfig,
-  jsConfig,
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      '@stylistic/ts': stylisticTs,
+    },
+    rules: {
+      '@stylistic/ts/semi': 'error',
+
+      // I like my template expressions, tyvm
+      '@typescript-eslint/restrict-template-expressions': 'off',
+
+      // and sometimes you gotta use any
+      '@typescript-eslint/no-explicit-any': 'off',
+
+      // these 6 bytes add up
+      '@typescript-eslint/require-await': 'off',
+
+      // unfortunately required when using Sets and Maps
+      '@typescript-eslint/no-non-null-assertion': 'off',
+
+      // this rule seems broken
+      '@typescript-eslint/no-invalid-void-type': 'off',
+
+      '@typescript-eslint/no-unnecessary-boolean-literal-compare': [
+        'error',
+        {
+          allowComparingNullableBooleansToTrue: true,
+          allowComparingNullableBooleansToFalse: true,
+        },
+      ],
+      '@typescript-eslint/unified-signatures': [
+        'error',
+        {
+          ignoreDifferentlyNamedParameters: true,
+        },
+      ],
+      // too many false positives
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          disallowTypeAnnotations: true,
+          fixStyle: 'inline-type-imports',
+          prefer: 'type-imports',
+        },
+      ],
+
+      '@typescript-eslint/consistent-type-exports': [
+        'error',
+        { fixMixedExportsWithInlineTypeSpecifier: true },
+      ],
+
+      '@stylistic/ts/lines-around-comment': [
+        'warn',
+        {
+          beforeBlockComment: true,
+          // these conflict with prettier, so we must allow them
+          allowObjectStart: true,
+          allowClassStart: true,
+          allowInterfaceStart: true,
+          allowBlockStart: true,
+          allowArrayStart: true,
+        },
+      ],
+    },
+  },
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    files: ['**/*.js', '**/*.cjs', '*.cjs', '*.js'],
+    extends: [tseslint.configs.disableTypeChecked],
+    rules: {
+      '@stylistic/js/semi': 'error',
+      '@stylistic/js/lines-around-comment': [
+        'warn',
+        {
+          beforeBlockComment: true,
+          // these conflict with prettier, so we must allow them
+          allowObjectStart: true,
+          allowClassStart: true,
+        },
+      ],
+    },
+  },
   {
     files: ['src/test/**/*.spec.ts'],
     rules: {
