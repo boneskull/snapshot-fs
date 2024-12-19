20457281ab583c5ceeb200cd4407c1bf6e92bef9
Christopher Hiller <boneskull@boneskull.com>
2024-12-19 13:07:27 -0800

    chore: update eslint config; add eslint-plugin-perfectionist

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,24 @@
 // @ts-check
 
+import eslint from '@eslint/js';
 import stylisticJs from '@stylistic/eslint-plugin-js';
 import stylisticTs from '@stylistic/eslint-plugin-ts';
 import eslintPluginJsonc from 'eslint-plugin-jsonc';
+import nodePlugin from 'eslint-plugin-n';
+import perfectionist from 'eslint-plugin-perfectionist';
 import tseslint from 'typescript-eslint';
 
-// TODO: setup eslint-plugin-n
 export default tseslint.config(
+  eslint.configs.recommended,
+  perfectionist.configs['recommended-natural'],
+  nodePlugin.configs['flat/recommended-module'],
   ...tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
       parserOptions: {
+        extraFileExtensions: ['.json5', '.jsonc'],
         project: './.config/tsconfig.eslint.json',
         tsconfigRootDir: import.meta.dirname,
-        extraFileExtensions: ['.json5', '.jsonc'],
       },
     },
   },
@@ -23,38 +28,25 @@ export default tseslint.config(
       '@stylistic/ts': stylisticTs,
     },
     rules: {
+      '@stylistic/ts/lines-around-comment': [
+        'warn',
+        {
+          allowArrayStart: true,
+          allowBlockStart: true,
+          allowClassStart: true,
+          allowInterfaceStart: true,
+          // these conflict with prettier, so we must allow them
+          allowObjectStart: true,
+          beforeBlockComment: true,
+        },
+      ],
+
       '@stylistic/ts/semi': 'error',
 
-      // I like my template expressions, tyvm
-      '@typescript-eslint/restrict-template-expressions': 'off',
-
-      // and sometimes you gotta use any
-      '@typescript-eslint/no-explicit-any': 'off',
-
-      // these 6 bytes add up
-      '@typescript-eslint/require-await': 'off',
-
-      // unfortunately required when using Sets and Maps
-      '@typescript-eslint/no-non-null-assertion': 'off',
-
-      // this rule seems broken
-      '@typescript-eslint/no-invalid-void-type': 'off',
-
-      '@typescript-eslint/no-unnecessary-boolean-literal-compare': [
+      '@typescript-eslint/consistent-type-exports': [
         'error',
-        {
-          allowComparingNullableBooleansToTrue: true,
-          allowComparingNullableBooleansToFalse: true,
-        },
+        { fixMixedExportsWithInlineTypeSpecifier: true },
       ],
-      '@typescript-eslint/unified-signatures': [
-        'error',
-        {
-          ignoreDifferentlyNamedParameters: true,
-        },
-      ],
-      // too many false positives
-      '@typescript-eslint/no-unnecessary-condition': 'off',
 
       '@typescript-eslint/consistent-type-imports': [
         'error',
@@ -65,42 +57,58 @@ export default tseslint.config(
         },
       ],
 
-      '@typescript-eslint/consistent-type-exports': [
-        'error',
-        { fixMixedExportsWithInlineTypeSpecifier: true },
-      ],
+      // and sometimes you gotta use any
+      '@typescript-eslint/no-explicit-any': 'off',
 
-      '@stylistic/ts/lines-around-comment': [
-        'warn',
+      // this rule seems broken
+      '@typescript-eslint/no-invalid-void-type': 'off',
+
+      // unfortunately required when using Sets and Maps
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-unnecessary-boolean-literal-compare': [
+        'error',
         {
-          beforeBlockComment: true,
-          // these conflict with prettier, so we must allow them
-          allowObjectStart: true,
-          allowClassStart: true,
-          allowInterfaceStart: true,
-          allowBlockStart: true,
-          allowArrayStart: true,
+          allowComparingNullableBooleansToFalse: true,
+          allowComparingNullableBooleansToTrue: true,
         },
       ],
+      // too many false positives
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+
+      // these 6 bytes add up
+      '@typescript-eslint/require-await': 'off',
+
+      // I like my template expressions, tyvm
+      '@typescript-eslint/restrict-template-expressions': 'off',
+
+      '@typescript-eslint/unified-signatures': [
+        'error',
+        {
+          ignoreDifferentlyNamedParameters: true,
+        },
+      ],
+
+      // fully broken
+      'n/no-missing-import': 'off',
     },
   },
   {
+    extends: [tseslint.configs.disableTypeChecked],
+    files: ['**/*.js', '**/*.cjs', '*.cjs', '*.js'],
     plugins: {
       '@stylistic/js': stylisticJs,
     },
-    files: ['**/*.js', '**/*.cjs', '*.cjs', '*.js'],
-    extends: [tseslint.configs.disableTypeChecked],
     rules: {
-      '@stylistic/js/semi': 'error',
       '@stylistic/js/lines-around-comment': [
         'warn',
         {
-          beforeBlockComment: true,
+          allowClassStart: true,
           // these conflict with prettier, so we must allow them
           allowObjectStart: true,
-          allowClassStart: true,
+          beforeBlockComment: true,
         },
       ],
+      '@stylistic/js/semi': 'error',
     },
   },
   {
@@ -117,5 +125,11 @@ export default tseslint.config(
   eslintPluginJsonc.configs['flat/prettier'][2],
   {
     ignores: ['dist', 'coverage', '__snapshots__', '.tshy*'],
+  },
+  {
+    files: ['src/cli.ts'],
+    rules: {
+      'n/hashbang': 'off',
+    },
   },
 );
