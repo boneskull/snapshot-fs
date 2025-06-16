ea120067c354bbd56d8ff71dad5e971ea9a3b1b1
Christopher Hiller <boneskull@boneskull.com>
2025-06-16 16:37:06 -0700

    chore: use tsup
    This replaces `tshy` with [tsup](https://tsup.egoist.dev) because I wanted to try it.  Unfortunately, it entrains `@microsoft/api-extractor` to use `tsup`'s own declaration-generating facilities because it is a [pain in the ass](https://mayank.co/blog/dual-packages/) to make `tsc` do this.  On the plus side, it seems to take care of broken global Node.js incompatibilities for us (so far).
    
    It also migrates `@stylistic/eslint-plugin-js` & `@stylistic/eslint-plugin-ts` to `@stylistic/eslint-plugin`.

--- test/test-root.ts
+++ test/test-root.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-
 import { fileURLToPath } from 'url';
 
-// @ts-ignore - for tshy
 export const TEST_ROOT = fileURLToPath(new URL('.', import.meta.url));
